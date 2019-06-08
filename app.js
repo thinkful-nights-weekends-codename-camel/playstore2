7b20ea07375dffea1f8332971eee386a9ce0698e
computershawn <shontek@gmail.com>
2019-06-08 12:56:35 -0700

    Completed basic API functionality

--- app.js
+++ app.js
@@ -1,42 +1,56 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
+
+
+app.use(cors());
+app.listen(8000, () => {
+    console.log("Server started on PORT 8000");
+})
 
 app.use(morgan('common'));
 const playstore = require('./playstore.js');
 
 app.get('/playstore', (req, res) => {
-  const { search = "", sort } = req.query;
+  const { genre, sort } = req.query;
+  
+  let allowableSortMethods = ['app', 'rating'];
+  let allowableFilterMethods = ['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'];
 
   if (sort) {
-    if (!['app', 'rating', 'genre'].includes(sort)) {
+    if (!allowableSortMethods.includes(sort)) {
       return res
         .status(400)
-        .send('Sort must be one of rating/app or genre');
+        .send('Sort must be one of rating or app');
     }
   }
 
-  let results = playstore
-    .filter(playstore =>
-      playstore
-        .app
-        .rating
-        .genre
-        .toLowerCase()
-        .includes(search.toLowerCase()));
-
-  if (sort) {
-    results
-      .sort((a, b) => {
-        return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
-      });
-      
+  if (genre) {
+    if (!allowableFilterMethods.includes(genre)) {
+      return res
+        .status(400)
+        .send('Genre must be either Action, Puzzle, Strategy, Casual, Arcade or Card');
+    }
   }
 
-  app.get('/', (req,res)=>{
-    res.redirect('/playstore') 
-  })
+  let results = playstore;
+  if (sort) {
+    let sortTerm;// = 'App';
+    if(sort === 'rating') {
+      sortTerm = 'Rating';
+    } else {
+      sortTerm = 'App';
+    }
+    results = playstore
+      .sort((a, b) => {
+        return a[sortTerm] > b[sortTerm] ? 1 : a[sortTerm] < b[sortTerm] ? -1 : 0;
+      });      
+  }
 
-  res
-  .json(results);
+  let filteredResults = results;
+  if (genre) {
+    filteredResults = results.filter(appItem => appItem.Genres.toLowerCase().includes(genre));
+  }
+  res.json(filteredResults);
 });
